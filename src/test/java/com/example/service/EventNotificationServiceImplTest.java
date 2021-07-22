48fa7ca706f24e7df561c3a8304cac4faf0d7a68
Ricardo <rnogueradrum@gmail.com>
2021-07-22 23:16:51 +0200

    commit EventNotificationServiceTest Attendees announce

--- src/test/java/com/example/service/EventNotificationServiceImplTest.java
+++ src/test/java/com/example/service/EventNotificationServiceImplTest.java
@@ -68,5 +68,70 @@ class EventNotificationServiceImplTest {
         }
     
     
+    @Order(8)
+    @Test
+    @DisplayName("Attendees announce Test")
+    void announceAttendeesTest() {
+    	myEvent.addAttendee(asistente1);
+        myEvent.addAttendee(asistente2);
+        
+        assertEquals(2,myEvent.getAttendees().size());
+        
+    	assertNotNull(myEvent);
+        assertNotNull(myEvent.getAttendees());
+        assertFalse(myEvent.getAttendees().isEmpty());
+        
+        myEvent.notifyAssistants();
+        
+        for (Attendee attendee : myEvent.getAttendees()) { 
+            assertNotNull(attendee.getNotifications());
+            //System.out.println(attendee.getNotifications().get(0).getMessage());
+        	
+        }
+    }
     
+    
+    @Order(10)
+    @DisplayName("If Event null ConfirmAttendance Test")
+	@Test
+	void confirmAttendanceIfEventNullTest() {
+    	myEvent = null;
+        assertNull(myEvent);
+        
+        assertNotNull(asistente1);
+    	
+    	eventNotificationService.confirmAttendance(myEvent, asistente1);
+    	assertNull(myEvent);
+		
+	}
+    
+    @Order(12)
+    @DisplayName("If Attendee null ConfirmAttendance Test")
+	@Test
+	void confirmAttendanceIfAttendeeNullTest() {
+    	
+    	assertNotNull(myEvent);
+    	
+    	asistente1 = null;
+        assertNull(asistente1);
+    	
+    	eventNotificationService.confirmAttendance(myEvent, asistente1);
+    	assertNull(asistente1);
+		
+	}
+    
+    @Order(14)
+    @DisplayName("If valid Event and Attendee ConfirmAttendance Test")
+	@Test
+	void confirmAttendanceValidTest() {
+    	
+    	assertNotNull(myEvent);
+  
+        assertNotNull(asistente1);
+    	
+    	eventNotificationService.confirmAttendance(myEvent, asistente1);
+    	assertNotNull(asistente1.getNotifications().get(0).getMessage());
+		System.out.println(asistente1.getNotifications().get(0).getMessage());
+	}
+
 }
