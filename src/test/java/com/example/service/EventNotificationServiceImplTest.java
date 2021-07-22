383397a38b4f2e291bf34e051c299ca1f72b3a04
Ricardo <rnogueradrum@gmail.com>
2021-07-22 23:13:44 +0200

    commit EventNotificationServiceTest I Event Null

--- src/test/java/com/example/service/EventNotificationServiceImplTest.java
+++ src/test/java/com/example/service/EventNotificationServiceImplTest.java
@@ -1,43 +1,47 @@
 package com.example.service;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import com.example.Attendee;
 import com.example.Event;
 import com.example.EventType;
+import com.example.Notification;
 
+
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class EventNotificationServiceImplTest {
 	
 	EventNotificationService eventNotificationService = new EventNotificationServiceImpl();
-	//Crear una evento
-    Event myEvent = new Event(1L, "EventoTic", EventType.TECH, eventNotificationService);//Crea evento
 	
-    @BeforeEach
-	void datosComunes() throws Exception {
-		
-        Attendee Asistente1 = new Attendee(1L, "Juan", "asistente1@gmail.com");
-        Attendee Asistente2 = new Attendee(2L, "Carmen", "asistente2@gmail.com");
-        myEvent.addAttendee(Asistente1);
-        myEvent.addAttendee(Asistente2);
-        
-        assertEquals(2,myEvent.getAttendees().size());
-        
-        myEvent.notifyAssistants();
-		
-	}
-
-	@Test
-	void testAnnounce() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testConfirmAttendance() {
-		fail("Not yet implemented");
-	}
+	
+    Event myEvent = new Event(1L, "EventoTic", EventType.TECH, eventNotificationService);//Crea evento
+    
+    Attendee asistente1 = new Attendee(1L, "Juan", "asistente1@gmail.com");
+    Attendee asistente2 = new Attendee(2L, "Carmen", "asistente2@gmail.com");
+    
+  
+    @Order(2)
+    @Test
+    @DisplayName("If Event null announce Test")
+    void announceIfEventNullTest() throws NullPointerException {
+    	   	
+    	myEvent = null;
+        assertNull(myEvent);
+       
+    	eventNotificationService.announce(myEvent);
+    	assertNull(myEvent);
+        }
+    
+    
 
 }
